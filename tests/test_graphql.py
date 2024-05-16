09d9762a9a778a3b477e76a2b7d596ae7a4ca794
Vee Satayamas <vsatayamas@gmail.com>
2024-05-16 10:40:48 +0700

    fix: polish

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -2,30 +2,24 @@
 GraphQL querying tests
 """
 import pytest
+from typing import Any
+from pytest_mock_resources import create_postgres_fixture
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session as scoped_session_factory
+from tugastugas.database import Base
 from tugastugas import schema
 from tugastugas.models import Project
-from tugastugas.database import Base
-from pytest_mock_resources import create_postgres_fixture
-from sqlalchemy import text
 
-alembic_engine = create_postgres_fixture()
 
-def session_fn(session):
-    session.add(Project(title='fake-proj-1'))
-    session.flush()
-    session.commit()
-
-#pg_session = create_postgres_fixture(Base, session_fn, session=True)
+alembic_engine: Any = create_postgres_fixture()
 pg_engine = create_postgres_fixture(Base)
 
+
 @pytest.mark.alembic()
-def test_project_query(alembic_runner, pg_engine):
+def test_project_query(alembic_runner: Any, pg_engine: Any) -> None:
     alembic_runner.migrate_up_to("head", return_current=False)
-
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy.orm import scoped_session as scoped_session_factory
-
-    session_factory = sessionmaker(autocommit=False, autoflush=False, bind=pg_engine)
+    session_factory = sessionmaker(autocommit=False, autoflush=False,
+                                   bind=pg_engine)
     pg_session = scoped_session_factory(session_factory)
     Base.query = pg_session.query_property()
 
@@ -42,6 +36,3 @@ def test_project_query(alembic_runner, pg_engine):
     Base.query = pg_session.query_property()
     result = schema.schema.execute(query)
     assert result.data['projects'][0]['title'] == 'A1'
- 
-
-
